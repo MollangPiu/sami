d4cd30f5a55e26a5a0374265e143e0478568d753
Mollang <hayaruby7909@gmail.com>
2022-06-25 14:07:40 +0900

    first Data

---
+++ semi_project/src/test/java/kr/soft/semi_project/SemiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.soft.semi_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

22f9de54eec15e2216ad9f96bf2b7bd41dd0739f
Mollang <hayaruby7909@gmail.com>
2022-06-25 12:17:49 +0900

    first Data

---
+++ sami_project/src/main/java/kr/co/soft/sami_project/SamiProjectApplication.java
@@ -0,0 +1,13 @@
+package kr.co.soft.sami_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamiProjectApplication.class, args);
+	}
+
+}
---
+++ sami_project/src/test/java/kr/co/soft/sami_project/SamiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.soft.sami_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
